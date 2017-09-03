f3f24cc7d6207378784ea9ea242d28cc63e634b0
boina-n <nboina.Eext@orange.com>
2017-09-03 22:36:41 +0000

    add csv output support

--- cf-all-routes.go
+++ cf-all-routes.go
@@ -13,12 +13,18 @@ func (c *AllRoutesPlugin) Run(cliConnection plugin.CliConnection, args []string)
 	if args[0] == "all-routes" {
 
 		var apiURL interface{}
+
+
 		apiURL = "/v2/spaces"
 
-		json, _ := cfcurl.Curl(cliConnection, apiURL.(string))
-		resources := toJSONArray(json["resources"])
+		items := []string{}
 
-		for _, spaceIntf := range resources {
+		items = append(items,"hostname,domain name,app")
+
+		json, _ := cfcurl.Curl(cliConnection, apiURL.(string))
+		space_resources := toJSONArray(json["resources"])
+
+		for _, spaceIntf := range space_resources {
 			space := toJSONObject(spaceIntf)
 			metadata := toJSONObject(space["metadata"])
 
@@ -29,13 +35,27 @@ func (c *AllRoutesPlugin) Run(cliConnection plugin.CliConnection, args []string)
 			routes_urls := entity["routes_url"]
 
 
+
 			json1, _ := cfcurl.Curl(cliConnection, routes_urls.(string))
-			resources := toJSONArray(json1["resources"])
-			for _, routeIntf := range resources {
+			routes_resources := toJSONArray(json1["resources"])
+
+			for _, routeIntf := range routes_resources {
 				route := toJSONObject(routeIntf)
 				entity := toJSONObject(route["entity"])
 				host := entity["host"]
 				domainguid := entity["domain_guid"]
+				apps_url := entity["apps_url"]
+
+				json3, _ := cfcurl.Curl(cliConnection, apps_url.(string))
+				apps_url_resources := toJSONArray(json3["resources"])
+
+				var app_name string
+				for _, appsIntf := range apps_url_resources {
+					apps := toJSONObject(appsIntf)
+					entity := toJSONObject(apps["entity"])
+					app_name = entity["name"].(string)
+				}
+				//fmt.Print(app_name)
 
 
 				apiURL="/v2/domains/" +domainguid.(string)
@@ -43,11 +63,21 @@ func (c *AllRoutesPlugin) Run(cliConnection plugin.CliConnection, args []string)
 				json, _  := cfcurl.Curl(cliConnection, apiURL.(string))
 				entity2 := toJSONObject(json["entity"])
 				domain := entity2["name"]
+				//items = append(items)
+				var record interface{}
+				record = host.(string)+","+domain.(string)+","+app_name
+				items = append(items,record.(string))
 
-				fmt.Print(host,".",domain,"\n")
+				//fmt.Print(host,",",domain,",",app_name,"\n")
 			}
 
 		}
+
+		for _,i := range items {
+			fmt.Println(i)
+		}
+
+
 }
 
 }
@@ -80,6 +110,8 @@ func (c *AllRoutesPlugin) GetMetadata() plugin.PluginMetadata {
 func main() {
 	plugin.Start(new(AllRoutesPlugin))
 }
+
+
 
 func toJSONArray(obj interface{}) []interface{} {
 	return obj.([]interface{})
